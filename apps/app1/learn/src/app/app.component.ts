2b3d034dddbcd4281b21c45447c2356d6f8f1ca9
Louaie <macosm2@MacBook-Pro-de-macosm2.local>
2023-06-19 22:14:12 +0100

    feat: init workspace

---
+++ apps/app1/learn/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent],
+  selector: 'learn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  title = 'app1-learn';
+}
